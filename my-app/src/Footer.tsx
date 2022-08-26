b43ce50be33cd3637bae05f7b28f40dae74dee19
Rasaboun <rasaboun@student.42.fr>
2022-08-26 06:10:56 +0200

    Change Footer Color

--- my-app/src/Footer.tsx
+++ my-app/src/Footer.tsx
@@ -4,7 +4,7 @@ import './output.css';
 export default function Footer() {
 	return (
 	  
-<footer className="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
+<footer className="mx-8 p-4 bg-white rounded-lg md:flex md:items-center md:justify-between md:p-6 dark:bg-brown-700">
     <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="#" className="hover:underline">Transcendance Team</a>. All Rights Reserved.
     </span>
     <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
