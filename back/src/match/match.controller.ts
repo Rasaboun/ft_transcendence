475d15910b017dfe13d6956908d52de9a50cd219
bditte <bditte@student.42.fr>
2022-09-28 17:57:57 +0200

    fixed get user games with login

--- back/src/match/match.controller.ts
+++ back/src/match/match.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, ParseIntPipe, Post, UseFilters, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseIntPipe, Post, Query, UseFilters, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
 //import { AuthenticatedGuard } from 'src/auth/guards/auth.guard';
 import { Match } from 'src/typeorm';
 import { matchDto } from './dto/match.dto';
@@ -12,17 +12,11 @@ export class MatchController {
 
     @Post('result')
     matchResult(@Body() resultDto: matchDto) {
-        console.log("Posting match :", resultDto);
         return this.matchService.matchResult(resultDto);
     }
 
-    @Get(':id')
-    findOneById(@Param('id', ParseIntPipe) id: number): Promise<Match> {
-        return this.matchService.findOneById(id);
-    }
-
     @Get('user')
-    getMatchsByUsername(@Body() dto: {login: string}): Promise<Match[]> {
+    getMatchesByLogin(@Query() dto: {login: string}): Promise<Match[]> {
         return this.matchService.getMatchesByLogin(dto.login);
     }
 
