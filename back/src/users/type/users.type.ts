1197e5a523faacca578b567b37a97c6ee9fa42dc
bditte <bditte@student.42.fr>
2022-10-13 23:43:52 +0200

    added GameStats in back

--- back/src/users/type/users.type.ts
+++ back/src/users/type/users.type.ts
@@ -1,3 +1,5 @@
+import { StringSchema } from "joi";
+
 export enum UserStatus {
     offline,
     online,
@@ -8,4 +10,30 @@ export type Friend = {
     login: string,
     username: string,
     status: UserStatus,
+}
+
+export type GameStats = {
+    victories: number,
+    defeats: number,
+    nbGames: number,
+    goalsScored: number,
+    goalsTaken: number,
+}
+
+export type MatchInfo = {
+    winnerLogin: string,
+    loserLogin: string,
+    winnerScore: number,
+    loserScore: number;
+}
+
+export function initGameStats(): GameStats
+{
+    return {
+        victories: 0,
+        defeats: 0,
+        goalsScored: 0,
+        goalsTaken: 0,
+        nbGames: 0,
+    }
 }
