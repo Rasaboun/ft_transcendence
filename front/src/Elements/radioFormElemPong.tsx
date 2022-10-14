3961fa2068cbe5af470c7ca51b2380ed856a0ec1
Rasaboun <rasaboun@student.42.fr>
2022-10-14 18:19:22 +0200

    FIX ERROR CONSOLE + STYLE RADIO PONG

---
+++ front/src/Elements/radioFormElemPong.tsx
@@ -0,0 +1,37 @@
+import React, { useContext, useState } from "react";
+import { SocketContext } from "../Context/socketContext";
+import { GameMode } from "../game/GameUtils/type";
+
+type PropsT = {
+  choice: any;
+  options: string[];
+  setChoice: (choice: any) => void;
+};
+
+export default function RadioFormElemPong(props: PropsT) {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    props.setChoice(parseFloat(e.target.value));
+  };
+
+  const OptionsElem = props.options.map((elem, idx) => (
+    <li key={elem} className="w-full border-b border-indigo-200 sm:border-b-0 sm:border-r ">
+      <div className="flex items-center pl-3 bg-indigo-400 border rounded-lg">
+        <input
+		  className="w-4 h-4 accent-sky-600"
+          name="mode"
+          type="radio"
+          value={idx}
+          checked={props.choice === idx}
+          onChange={handleChange}
+        />
+        <label className="py-3 ml-2 w-full text-sm font-medium text-gray-900" key={idx}>{elem}</label>
+      </div>
+    </li>
+  ));
+
+  return (
+    <ul className="items-center w-full text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 sm:flex">
+      {OptionsElem}
+    </ul>
+  );
+}
